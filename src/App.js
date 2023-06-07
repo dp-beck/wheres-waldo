59a12f1ebadfd1502009e1b7871238ee11bb6d9e
Daniel Beck <beckdp01@gmail.com>
2023-06-07 06:58:05 -0400

    Add router to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import { BrowserRouter, Routes, Route, } from "react-router-dom";
+import Home from './Home';
+import Game from './Game';
+import Leaderboard from './Leaderboard';
+import NotFound from './NotFound';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/ancient-greece" element={<Game />} />
+          <Route path="/beach" element={<Game />}/>
+          <Route path="/castle" element={<Game />} />
+          <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="*" element={<NotFound />} /> 
+        </Routes>
+      </BrowserRouter>
+      );
+  }
+  
+  export default App;
+  
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import './Home.css';
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>Home</div>
+    );
+}
+
+export default Home;
