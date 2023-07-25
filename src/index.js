f7bcacb661edeb4dca4d547dfe289ee4b8d393ac
Daniel Beck <beckdp01@gmail.com>
2023-07-24 20:35:41 -0400

    update router

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDH_kFcoICJfF8JoVPaeLwQDuFvWtLUXMs",
@@ -24,7 +24,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <HashRouter basename="/">
+        <App/>
+      </HashRouter>
     </BrowserRouter>
   </React.StrictMode>
 );
