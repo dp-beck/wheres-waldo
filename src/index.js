47c93c28e103bd5c1a21f4672bf8b29e09110db4
Daniel Beck <beckdp01@gmail.com>
2023-07-24 21:19:09 -0400

    reverted to before gh pages setup

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDH_kFcoICJfF8JoVPaeLwQDuFvWtLUXMs",
@@ -24,7 +24,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-        <App />
+      <App />
   </BrowserRouter>
   </React.StrictMode>
 );
